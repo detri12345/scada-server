646a019e8cadee46a8165482b8eededc305147aa
detri12345 <64020434+detri12345@users.noreply.github.com>
2026-04-13 20:21:30 +0700

    Update server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let latestData = {};
+
+app.get("/", (req, res) => {
+  res.send("SCADA Server Online 🚀");
+});
+
+app.post("/api/data", (req, res) => {
+  latestData = req.body;
+  res.send({ status: "ok" });
+});
+
+app.get("/api/data", (req, res) => {
+  res.send(latestData);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server jalan di port", PORT);
+});
